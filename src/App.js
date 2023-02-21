f771a0774f3880569cd3c17bc4dbea812c94cd3b
Saurabh Singh <saurabhsingh@classicinformatics.com>
2023-02-21 23:38:05 +0530

    Done with login and video play back and some css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Switch } from "react-router";
+
+import Login from "./components/Login";
+import Header from "./components/Header";
+import Home from "./components/Home";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Login />}></Route>
+            <Route path="/home" element={<Home />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </>
     </div>
   );
 }
